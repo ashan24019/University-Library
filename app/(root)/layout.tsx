4bf23648e32dd2e2e95aaadfea40cb33f9090ca5
Ashan <ashanvimodh241@gmail.com>
2025-04-06 20:13:13 +0530

    Initialize university library project

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React,{ ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <div className="mx-auto max-w-7xl flex flex-col items-center">
+        Header
+        <div className=" mx-auto mt-20 pb-20">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => (
+  <>
+    <Button className="bg-[#E7C9A5]">Click Me</Button>
+  </>
+);
+
+export default Home;
