f30a02354c29404f58cb296f7b5d6852c9bfe3f3
Ashan <ashanvimodh241@gmail.com>
2025-04-12 11:52:49 +0530

    Add book cover  but the look I want didnt appear

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -15,47 +15,47 @@ const BookOverview = ({
   cover,
 }: Book) => {
   return (
-    <section className="text-5xl font-semibold text-white md:text-7xl">
-      <div className="flex flex-1 flex-col gap-5">
-        <h1>{title}</h1>
+    <section className="flex flex-col-reverse md:flex-row items-start justify-between gap-10">
+      {/* LEFT SIDE - Book Details */}
+      <div className="flex flex-1 flex-col gap-5 text-white">
+        <h1 className="text-5xl font-semibold md:text-7xl">{title}</h1>
 
-        <div className="mt-7 flex flex-row flex-wrap gap-4 text-xl text-[#D6E0FF]">
+        <div className="mt-4 flex flex-row flex-wrap gap-4 text-xl text-[#D6E0FF]">
           <p>
             By <span className="font-semibold text-[#D9B28B]">{author}</span>
           </p>
-
           <p>
-            Category {""}
+            Category{" "}
             <span className="font-semibold text-[#D9B28B]">{genre}</span>
           </p>
         </div>
 
-        <div className="flex flex-row gap-1">
+        <div className="flex flex-row items-center gap-2 text-2xl">
           <Image src="/icons/star.svg" alt="star" width={22} height={22} />
-          <p className="text-2xl">{rating}</p>
+          <p>{rating}</p>
         </div>
 
-        <div className="text-2xl flex flex-row flex-wrap gap-4 mt-1">
+        <div className="text-2xl flex flex-row flex-wrap gap-4 mt-1 text-[#F5E6D5]">
           <p>
-            Total Books: <span className="text-[#F5E6D5]">{totalCopies}</span>
+            Total Books: <span>{totalCopies}</span>
           </p>
-
           <p>
-            Available Books:{" "}
-            <span className="text-[#F5E6D5]">{availableCopies}</span>
+            Available Books: <span>{availableCopies}</span>
           </p>
         </div>
+
         <p className="mt-2 text-justify text-xl text-[#F5E6D5]">
           {description}
         </p>
 
         <Button className="mt-4 min-h-14 w-fit bg-[#E7C9A5] text-[#16191E] hover:bg-[#E7C9A5]/80 max-md:w-full">
           <Image src="/icons/book.svg" alt="book" width={20} height={20} />
-          <p className="font-bebas-neue text-[#16191E] text-">Borrow Book</p>
+          <p className="ml-2 font-bebas-neue text-lg">BORROW BOOK REQUEST</p>
         </Button>
       </div>
 
-      <div className="relative flex flex-1 justify-center">
+      {/* RIGHT SIDE - Book Cover */}
+      <div className="relative flex flex-1 justify-center md:justify-end">
         <div className="relative">
           <BookCover
             variant="wide"
@@ -63,16 +63,9 @@ const BookOverview = ({
             coverColor={color}
             coverImage={cover}
           />
-
           <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
-          <BookCover
-            variant="wide"
-            coverColor={color}
-            coverImage={cover}
-          />
+            <BookCover variant="wide" coverColor={color} coverImage={cover} />
           </div>
-
-
         </div>
       </div>
     </section>
