09ac81f9511c24b01d8bcfe5ccc6595176d05004
Ashan <ashanvimodh241@gmail.com>
2025-04-16 12:02:59 +0530

    creating authform

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+"use client";
 
-interface Props<T extends FieldValues>
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  DefaultValues,
+  FieldValues,
+  SubmitHandler,
+  useForm,
+  UseFormReturn,
+} from "react-hook-form";
+import { z, ZodType } from "zod";
 
-const AuthForm = ({type, schema, defaultValues, onSubmit}: Props) => {
-    return <div> </div>
+interface Props<T extends FieldValues> {
+  schema: ZodType<T>;
+  defaultValues: T;
+  onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
+  type: "SIGN_IN" | "SIGN_UP";
+}
+
+const AuthForm = <T extends FieldValues>({
+  type,
+  schema,
+  defaultValues,
+  onSubmit,
+}: Props<T>) => {
+  const form: UseFormReturn<T> = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
+
+  const handleSubmit: SubmitHandler<T> = async (data) => {};
+
+  return <div>AuthForm -- {type}</div>;
 };
 
 export default AuthForm;
