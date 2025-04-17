97e6bed496ac49f754a26f928252a8be48816095
Ashan <ashanvimodh241@gmail.com>
2025-04-18 01:09:07 +0530

    created image upload and intall image upload sdk 1.45

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+"use client;";
+
+import config from "@/lib/config";
+import { Image, ImageKitProvider } from "@imagekit/next";
+
+const authenticater = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
+    }
+    const data = await response.json();
+    const {signature, expire, token} = data;
+
+    return {token, expire, signature}
+
+  } catch (error: any) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
+};
 
 const ImageUpload = () => {
-  return (
-    <div>ImageUpload</div>
-  );
+  return <div>ImageUpload</div>;
 };
 
 export default ImageUpload;
