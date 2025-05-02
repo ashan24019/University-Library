6e9f59072888c5715142621e3933ebc2f0e10ba8
Ashan <ashanvimodh241@gmail.com>
2025-05-02 11:01:06 +0530

    ABle to display books on home page from the database

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 import React from "react";
-import BookCover from "./BookCover";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { Button } from "./ui/button";
+import BookCover from "./BookCover";
 
 const BookCard = ({
   id,
   title,
   genre,
-  color,
-  cover,
+  coverColor,
+  coverUrl,
   isLoanedBook = false,
 }: Book) => (
   <li className={cn(isLoanedBook && "xs:w-52 w-full")}>
@@ -18,7 +18,7 @@ const BookCard = ({
       href={`/book/${id}`}
       className={cn(isLoanedBook && "w-full flex flex-col items-center")}
     >
-      <BookCover coverColor={color} coverImage={cover} />
+      <BookCover coverColor={coverColor} coverImage={coverUrl} />
       <div className={cn("mt-4", !isLoanedBook && "xs:max-w-40 max-w-28")}>
         <p className="book-title">{title}</p>
         <p className="book-genre">{genre}</p>
